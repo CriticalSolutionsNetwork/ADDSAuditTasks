09699b135c7029c0140424af210757844fa1ff55
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2022-11-21 16:36:45 -0600

    docs: testing oui lookup

--- source/Public/Get-NetworkScan.ps1
+++ source/Public/Get-NetworkScan.ps1
@@ -57,6 +57,7 @@ function Get-NetworkScan {
                 "445", "389", "636", "514", "587", "1701", `
                 "3268", "3269", "3389", "5985", "5986"
         }
+        $ouiobject= invoke-restmethod https://standards-oui.ieee.org/oui/oui.csv | ConvertFrom-Csv
     } # Begin Close
     process {
         if ($LocalSubnets) {
@@ -101,3 +102,42 @@ function Get-NetworkScan {
         return $results
     }# End Close
 }
+
+$testscan = Invoke-PSnmap -ComputerName "10.11.10.0/24" -Port $ports -Dns -NoSummary -AddService | Where-Object { $_.Ping -eq $true }
+$testscan | ForEach-Object {
+    $macid = ((arp -a $_.ComputerName | Select-String '([0-9a-f]{2}-){5}[0-9a-f]{2}').Matches.Value).Replace("-",":")
+    $macpop = $macid.replace(":","")
+    $macsubstr = $macpop.Substring(0,6)
+    $org = ($ouiobject | Where-Object {$_.assignment -eq $macsubstr })."Organization Name"
+    Add-Member -InputObject $_ -MemberType NoteProperty -Name MacID -Value $macid
+    Add-Member -InputObject $_ -MemberType NoteProperty -Name ManufacturerName -Value $org
+    Start-Sleep -Seconds 1
+}
+
+<#
+
+$A = Get-ChildItem C:\Temp\test.txt
+$S = {[math]::Round(($this.Length / 1MB), 2)}
+$A | Add-Member -MemberType ScriptMethod -Name "SizeInMB" -Value $S
+$A.SizeInMB()
+
+0.43
+# Test retrieval using arp
+$macid = ((arp -a $_.ComputerName | Select-String '([0-9a-f]{2}-){5}[0-9a-f]{2}').Matches.Value).Replace("-",":")
+$ouifile = invoke-restmethod https://standards-oui.ieee.org/oui/oui.csv
+# TestMAC id formatted the same as previous output for $macid. Replace with $testmacid with $macid in prod.
+$testmacid = "00:15:5D:00:00:00"
+# Format mac for search
+$macpop = $testmacid.replace(":","")
+# Convert CSV to Object
+$ouiobject = $ouifile | ConvertFrom-Csv
+# Select first 6 digits
+$macsubstr = $macpop.Substring(0,6)
+# search ieee oui doc for Organization
+($ouiobject | Where-Object {$_.assignment -eq $macsubstr })."Organization Name"
+
+$testoutput
+
+$testmacid
+
+#>
