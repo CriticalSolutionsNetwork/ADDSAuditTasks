3eb65b2b5bd7886408995dd501f1f8ba6a71c663
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2022-11-22 09:11:45 -0600

    fix: If statement for OUI not found. docs: Docs updated.

--- source/Public/Get-NetworkScan.ps1
+++ source/Public/Get-NetworkScan.ps1
@@ -85,7 +85,12 @@ function Get-NetworkScan {
                         $macsubstr = $macpop.Substring(0, 6)
                         $org = ($ouiobject | Where-Object { $_.assignment -eq $macsubstr })."Organization Name"
                         Add-Member -InputObject $_ -MemberType NoteProperty -Name MacID -Value $macid
-                        Add-Member -InputObject $_ -MemberType NoteProperty -Name ManufacturerName -Value $org
+                        if ($org) {
+                            Add-Member -InputObject $_ -MemberType NoteProperty -Name ManufacturerName -Value $org
+                        }
+                        else {
+                            Add-Member -InputObject $_ -MemberType NoteProperty -Name ManufacturerName -Value "Not Found"
+                        }
                     }
                     # Normalize Subnet text for filename.
                     $subnetText = $(($subnet.Replace("/", ".CIDR.")))
@@ -110,37 +115,3 @@ function Get-NetworkScan {
         return $results
     }# End Close
 }
-
-<#
-
-[int[]]$ports = "21", "22", "23", "25", "53", "67", "68", "80", "443", `
-    "88", "464", "123", "135", "137", "138", "139", `
-    "445", "389", "636", "514", "587", "1701", `
-    "3268", "3269", "3389", "5985", "5986"
-$testscan = Invoke-PSnmap -ComputerName "10.11.10.0/24" -Port $ports -Dns -NoSummary -AddService | Where-Object { $_.Ping -eq $true }
-
-$A = Get-ChildItem C:\Temp\test.txt
-$S = {[math]::Round(($this.Length / 1MB), 2)}
-$A | Add-Member -MemberType ScriptMethod -Name "SizeInMB" -Value $S
-$A.SizeInMB()
-
-0.43
-# Test retrieval using arp
-$macid = ((arp -a $_.ComputerName | Select-String '([0-9a-f]{2}-){5}[0-9a-f]{2}').Matches.Value).Replace("-",":")
-$ouifile = invoke-restmethod https://standards-oui.ieee.org/oui/oui.csv
-# TestMAC id formatted the same as previous output for $macid. Replace with $testmacid with $macid in prod.
-$testmacid = "00:15:5D:00:00:00"
-# Format mac for search
-$macpop = $testmacid.replace(":","")
-# Convert CSV to Object
-$ouiobject = $ouifile | ConvertFrom-Csv
-# Select first 6 digits
-$macsubstr = $macpop.Substring(0,6)
-# search ieee oui doc for Organization
-($ouiobject | Where-Object {$_.assignment -eq $macsubstr })."Organization Name"
-
-$testoutput
-
-$testmacid
-
-#>
